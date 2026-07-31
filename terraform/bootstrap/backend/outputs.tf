b0aa9d8261ef49e2100844bfe0f237363a0c6028
Rustam Ohly <adamsonrustam@gmail.com>
2026-07-31 11:51:48 +0200

    Add Terraform infrastructure foundation

---
+++ terraform/bootstrap/backend/outputs.tf
@@ -0,0 +1,23 @@
+output "terraform_state_bucket" {
+
+
+  description = "S3 bucket for Terraform state"
+
+
+  value = aws_s3_bucket.terraform_state.bucket
+
+
+}
+
+
+
+output "terraform_lock_table" {
+
+
+  description = "DynamoDB table for Terraform lock"
+
+
+  value = aws_dynamodb_table.terraform_lock.name
+
+
+}
---
+++ terraform/outputs.tf
@@ -0,0 +1,12 @@
+output "vpc_id" {
+
+  value = module.network.vpc_id
+
+}
+
+
+output "public_subnet_id" {
+
+  value = module.network.public_subnet_id
+
+}
